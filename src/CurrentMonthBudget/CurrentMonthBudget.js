1f9ba0948288117e12925fd77cd74c734b0972a2
Julie Broderick <broderick.julie@gmail.com>
2019-06-12 23:50:57 -0500

    started state in CurrentBudget

--- src/CurrentMonthBudget/CurrentMonthBudget.js
+++ src/CurrentMonthBudget/CurrentMonthBudget.js
@@ -3,53 +3,64 @@ import './CurrentMonthBudget.css'
 import { BudgetCategory } from '../BudgetCategory/BudgetCategory'
 
 export default class CurrentMonthBudget extends Component {
-  static defaultProps = {
-    categories: [],
-    currentBudget: []
-  }
+    static defaultProps = {
+        categories: [],
+        currentBudget: []
+    }
 
-  state = {
-    currentBudget: [
-      { category_id: 2, category_name: 'Groceries', amount: '60' }
-    ]
-  }
+    state = {
+        currentBudget: [
+            { category_id: 2, category_name: 'Groceries', amount: '60' }
+        ],
+        newCategoryBudget: {}
+    }
 
-  handleCategoryAmount(amount, id) {
-    const { currentBudget } = this.state
-    const budgetCatToChange = currentBudget.find(cat => cat.category_id === id)
-    budgetCatToChange.amount = amount
-    const newBudget = [budgetCatToChange, { ...currentBudget }]
-    this.setState({
-      currentBudget: newBudget
-    })
-  }
+    handleCategoryAmount(amount, id) {
+        const catObj = this.state.currentBudget.find(cat => cat.category_id === id)
+        catObj.amount = amount
+        this.setState({
+            newCategoryBudget: catObj
+        })
+    }
 
-  render() {
-    const displayCategories = this.state.currentBudget.map(cat => (
-      <BudgetCategory
-        enterCategoryAmount={this.handleCategoryAmount}
-        {...props}
-      />
-    ))
-    const left = this.props.income - budgeted
+    submitCategoryAmount = (e) => {
+        e.preventDefault()
+        const { currentBudget, newCategoryBudget } = this.state
+        const omittedChangedCategory = currentBudget.filter(cat => cat.category_id !== newCategoryBudget.category_id)
+        const newBudget = [omittedChangedCategory, { ...currentBudget }]
+        this.setState({
+            currentBudget: newBudget
+        })
+    }
 
-    return (
-      <section>
-        <h2>{`Current Month's Total Budget: ${this.props.income}`}</h2>
-        <form onSubmit={e => this.props.submitIncome(e)}>
-          <label>
-            How much money will you make this month?
-            <input
-              type="text"
-              onChange={e => this.props.enterIncome(e.target.value)}
+    render() {
+        const displayCategories = this.state.currentBudget.map(cat => (
+            <BudgetCategory
+                handleCategoryAmount={this.handleCategoryAmount}
+                submitCategoryAmount={this.submitAmount}
+                {...cat}
             />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-        <h2>Your Budget Categories</h2>
-        <h3>{`Amount Budgeted: ${budgeted} | Amount Left to Budget: ${left}`}</h3>
-        <ul>{displayCategories}</ul>
-      </section>
-    )
-  }
+        ))
+        const budgeted = this.state.currentBudget.reduce((a, b) => a.amount = b.amount)
+        const left = this.props.income - budgeted
+
+        return (
+            <section>
+                <h2>{`Current Month's Total Budget: ${this.props.income}`}</h2>
+                <form onSubmit={e => this.props.submitIncome(e)}>
+                    <label>
+                        How much money will you make this month?
+            <input
+                            type="text"
+                            onChange={e => this.props.enterIncome(e.target.value)}
+                        />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+                <h2>Your Budget Categories</h2>
+                <h3>{`Amount Budgeted: ${budgeted} | Amount Left to Budget: ${left}`}</h3>
+                <ul>{displayCategories}</ul>
+            </section>
+        )
+    }
 }
