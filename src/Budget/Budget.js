9cbc6d4111d8dc441c0808e2a880315c8d6a6c2f
Julie Broderick <broderick.julie@gmail.com>
2019-06-19 19:16:20 -0500

    start updating budget cat with Edit/Save button

--- src/Budget/Budget.js
+++ src/Budget/Budget.js
@@ -2,20 +2,24 @@ import React from 'react'
 import './Budget.css'
 
 function Budget(props) {
-    return (
-        <div>
-            <header>
-                <h2>Budget</h2>
-            </header>
-            <ul>
-                {props.budgetCategories.map(cat => <li key={cat.category_id}>{`${cat.category_name} | ${cat.amountSpent} | ${cat.amountBudgeted}`}</li>)}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <header>
+        <h2>Budget</h2>
+      </header>
+      <ul>
+        {props.budgetCategories.map(cat => (
+          <li key={cat.category_id}>{`${cat.category_name} | ${
+            cat.amountSpent
+          } | ${cat.amountBudgeted}`}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 Budget.defaultProps = {
-    budgetCategories: []
+  budgetCategories: []
 }
 
 export default Budget
