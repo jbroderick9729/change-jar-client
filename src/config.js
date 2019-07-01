fe26c768a66533e7dfe0b7b4b49f49cca129bff0
Julie Broderick <broderick.julie@gmail.com>
2019-06-30 21:16:40 -0500

    updating to use producton server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://shielded-headland-23923.herokuapp.com/api',
     TOKEN_KEY: 'chargejar-client-auth-token'
 }
